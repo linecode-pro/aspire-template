af22c3e6edd15862bfed5f6078cd47ee5044ad4c
Sergey Rudenko <linecodepro@gmail.com>
2025-03-18 21:04:55 +0400

    Добавьте файлы проекта.

---
+++ Bugslog2025.AppHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using Bugslog2025.AppHost;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddGarnet("cache")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var seq = builder.AddSeq("seq")
+                 .ExcludeFromManifest()
+                 .WithLifetime(ContainerLifetime.Persistent)
+                 .WithEnvironment("ACCEPT_EULA", "Y");
+
+var apiService = builder.AddProject<Projects.Bugslog2025_ApiService>("apiservice")
+    .WithSwaggerUI()
+    .WithReference(seq);
+
+builder.AddProject<Projects.Bugslog2025_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithReference(apiService)
+    .WaitFor(apiService)
+    .WithReference(seq)
+    .WaitFor(seq);
+
+builder.Build().Run();
---
+++ Bugslog2025.AppHost/ResourceBuilderExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugslog2025.AppHost
+{
+    internal static class ResourceBuilderExtensions
+    {
+        internal static IResourceBuilder<T> WithSwaggerUI<T>(this IResourceBuilder<T> builder)
+        where T : IResourceWithEndpoints
+        {
+            return builder.WithOpenApiDocs("swagger-ui-docs", "Swagger API Documentation", "swagger");
+        }
+
+        private static IResourceBuilder<T> WithOpenApiDocs<T>(this IResourceBuilder<T> builder,
+            string name,
+            string displayName,
+            string openApiUiPath)
+            where T : IResourceWithEndpoints
+        {
+            return builder.WithCommand(
+                name,
+                displayName,
+                executeCommand: async _ =>
+                {
+                    try
+                    {
+                        // Base URL
+                        var endpoint = builder?.GetEndpoint("https") ?? throw new Exception("Не задан endpoint https");
+
+                        var url = $"{endpoint.Url}/{openApiUiPath}";
+
+                        await Task.Run(() => Process.Start(new ProcessStartInfo(url) { UseShellExecute = true }));
+
+                        return new ExecuteCommandResult { Success = true };
+                    }
+                    catch (Exception ex)
+                    {
+                        return new ExecuteCommandResult { Success = false, ErrorMessage = ex.Message };
+                    }
+                },
+                updateState: context => context.ResourceSnapshot.HealthStatus == Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Healthy ?
+                    ResourceCommandState.Enabled : ResourceCommandState.Disabled,
+                iconName: "Document",
+                iconVariant: IconVariant.Filled);
+        }
+    }
+}
